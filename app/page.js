2c1d2a7c7f42ea7bb26a832ee9acbd589af7a635
Pedro Leite <pedrolte11@hotmail.com>
2025-02-03 22:19:41 -0300

    react carrinho
    Fizemos a atividade de um carrinho em tempo real, com novas funções gigantescas. Essa atividade é comparada a outra de carrinho que temos

--- app/page.js
+++ app/page.js
@@ -1,101 +1,67 @@
+'use client' // tem que colocar sempre pra chamar as funçõe do next de variaveis
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  
+  // Aqui entra o Javascript normal
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  let [ carrinho, alteraCarrinho] = useState(0);  // se for ingles, seria setCarrinho
+  let [ precoProduto, alteraPreco] = useState(25);
+  let [ valorTotal, alteraValorTotal] = useState(0);
+
+  
+  function manipulaCarrinho( adicionar ){ // é aqui que possibilitamos a manipulação do clique, pra fazer clicar e funcionar
+
+    let novoCarrinho = carrinho;
+
+    if(adicionar == true){
+      alteraCarrinho( carrinho + 1)
+      novoCarrinho++
+    }else{
+      alteraCarrinho( carrinho - 1)
+      novoCarrinho--
+    }
+    alteraValorTotal (novoCarrinho * precoProduto)
+  }
+  
+  function limpaCarrinho(){
+    alteraCarrinho (0)
+    alteraValorTotal (0)
+  }
+  
+  function manipulaPreco( ){
+    alteraPreco (19)
+  }
+
+  return (
+    <div className="p-5">
+      <h1 className="bg-sky-500 text-white p-3"> Lojinha do Next</h1>
+      <p className="p-3 text-lg"> 
+      Carrinho: <strong> {carrinho} </strong> Itens 
+      </p>
+      <button onClick={()=>limpaCarrinho()}  className="mr-2 bg-yellow-400 text-black mt-5 p-3 mb-3"> Limpar </button>
+      
+      <button onClick={()=>manipulaPreco()}  className="bg-black text-white mt-5 p-3 mb-3"> Adicionar Cupom </button>
+      
+      <hr/>
+
+      <p className="p-3 text-lg text-red-600"> 
+      Valor Total: R$ {valorTotal}  
+      </p>
+      <hr/>
+
+      <h2>Produtos</h2>
+
+      <div className=" border bg-sky-700 w-fit text-center text-white p-2">
+        <img className="" src="https://placehold.co/200/"/>
+        <h3 className="text-lime-200 font-bold">Produto modelo </h3>
+        <p>
+          R$ {precoProduto},00</p>
+        <button onClick={()=>manipulaCarrinho(true)} className="bg-lime-400 text-blac mt-5 p-3"> Adicionar ao carrinho </button>
+        <br/>
+        <button onClick={()=>manipulaCarrinho(false)} className="bg-red-400 text-blac mt-5 p-3"> Remover do Carrinho</button>
+      </div>
     </div>
   );
 }
